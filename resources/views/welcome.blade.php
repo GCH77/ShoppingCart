cdb991e6ab4d97d33bba7401a0e5b474cca6f17e
johanamerchan <leidyespinel05@gmail.com>
2019-03-26 22:11:50 -0500

    algov

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Compras
+                    vv
                 </div>
 
                 <div class="links">
